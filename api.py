1d329559d0adaad36cb549135fe6b2439afbd9b9
admin <root@local>
2018-01-28 14:38:30 +0100

    v3.7.5

--- api.py
+++ api.py
@@ -16,11 +16,11 @@ class API(object):
 		self.crypter=Crypter()
 		self.s=requests.session()
 		self.s.verify=False
-		self.s.headers.update({'User-Agent':'SMON_Kr/3.7.2.37200 CFNetwork/808.2.16 Darwin/16.3.0'})
+		self.s.headers.update({'User-Agent':'SMON_Kr/3.7.5.37500 CFNetwork/808.2.16 Darwin/16.3.0'})
 		if 'Admin-PC' == socket.gethostname():
 			self.s.proxies.update({'http': 'http://127.0.0.1:8888','https': 'https://127.0.0.1:8888',})
 		self.game_index=2623
-		self.proto_ver=11031
+		self.proto_ver=11040
 		self.app_version='3.7.2'
 		self.c2_api='http://summonerswar-gb.qpyou.cn/api/gateway_c2.php'
 		self.uid=int(uid)
