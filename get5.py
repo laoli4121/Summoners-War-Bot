26b38d4bfe5bc65c6b3466e9325707ba49232e53
laoli4121 <laoli4121@gmail.com>
2018-08-14 08:25:59 +0800

    modify get5

--- get5.py
+++ get5.py
@@ -13,20 +13,13 @@ while(True):
 		a.setRegion('sea')
 		a.setIDFA(Tools().rndDeviceId())
 		a.login()
-		mon_class, mon_id = a.auto_run()
-		if mon_class != 3:
-			mon_name = monster.monsters_name_map[str(mon_id)[0:3]]
-			f = open("lightMonster.txt", "a+")
-			f.write("%s, %s:%s %s  --%s\n" % (mon_class, mon_id, mon_name, uid, time.strftime('%H:%M')))
-			f.close()
-		a.doDungeonAndSellRune(8001, 1)
-		a.UpdateAchievement(a.makeList(203, 1, 1))
-		a.doDungeonAndSellRune(8001, 2)
-		a.doDungeonAndSellRune(8001, 3)
-		a.doDungeonAndSellRune(8001, 4)
-		a.UpdateAchievement(a.makeList(204, 1, 1))
-		a.doDungeonAndSellRune(8001, 5)
-		a.repeatDoDungeonAndSellRune(8001, 6)
+		for summon_mon in a.auto_run():
+			if summon_mon['mon_class'] != 3:
+				mon_name = monster.monsters_name_map[str(summon_mon['mon_id'])[0:3]]
+				f = open("lightMonster.txt", "a+")
+				f.write("%s, %s:%s %s  --%s\n" % (mon_class, mon_id, mon_name, uid, time.strftime('%H:%M')))
+				f.close()
+		a.repeatDoDungeonAndSellRune(8001, 10)
 		for i in range(10):
 			a.log("summoning...")
 			mon_class, mon_id = a.SummonLight(2)
